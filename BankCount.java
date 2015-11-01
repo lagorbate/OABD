0b0029c8903ae149f1a2df4129b114ca8498f213
lagorbate <geoffroygourbat@hotmail.fr>
2015-11-01 22:39:08 +0100

    Update BankCount.java

--- BankCount.java
+++ BankCount.java
@@ -1,6 +1,5 @@
 import java.io.IOException;
 import java.util.StringTokenizer;
-
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
@@ -14,35 +13,34 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 public class BankCount{
 
 public static class TokenizerMapper
-     extends Mapper<Object, Text, Text, IntWritable>{
-
+  extends Mapper<Object, Text, Text, IntWritable>{
   private final static IntWritable one = new IntWritable(1);
   private Text word = new Text();
-  private counter = 0;
+  private int counter = 0;//used to skip the first line (header)
 
-public void map(Object key, Text value, Context context
-                ) throws IOException, InterruptedException {
+  public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
               String[] tokens = value.toString().split(";");
               String location = tokens[1];
-              if(counter==0)tokens[3]="0";
               int account = Integer.parseInt(tokens[3]);
+              if(counter==0)//if processed line is the header
+	      {
+		location = " ";
+		account = 0;
+	      }
               counter++;
-              context.write(new Text(location), new IntWritable(account));
-       }
-    }
+              context.write(new Text(location),new IntWritable(account));
+  }
+}
 
 
 
-public static class Reduce
-  extends Reducer<Text, IntWritable, Text, IntWritable> {
-       public void reduce(Text key, Iterable<IntWritable> values,
-                          Context context
-                          ) throws IOException,InterruptedException {
+public static class Reduce extends Reducer<Text, IntWritable, Text, IntWritable> {
+       public void reduce(Text key, Iterable<IntWritable> values,Context context) throws IOException,InterruptedException {
               int sum = 0;
               for (IntWritable val : values) {
                    sum += val.get();
               }
-       context.write(key , new IntWritable(sum));
+              context.write(key , new IntWritable(sum));
        }
 }
 
